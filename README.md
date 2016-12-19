eb906a060521a2f7c23c2a7832ec231dcf10cff6
Neville Bonavia <neville.bonavia@betssongroup.com>
2016-12-19 19:41:19 +0100

    adding starter code

---
+++ README.md
@@ -0,0 +1,6 @@
+Request and response drills
+===========================
+
+Starter files for A/B test drill for Thinkful's Node course
+
+https://github.com/Thinkful-Ed/hyperdev-a-b-test-drill-starter
---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+const express = require('express');
+
+const app = express();
+
+
+// listen for requests :)
+app.listen(process.env.PORT || 8080, () => console.log(
+  `Your app is listening on port ${process.env.PORT || 8080 }`));

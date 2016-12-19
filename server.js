7fe3be0adeaeb6aa979a136807586d070050be9b
Neville Bonavia <neville.bonavia@betssongroup.com>
2016-12-19 20:25:58 +0100

    added solution

--- server.js
+++ server.js
@@ -2,9 +2,18 @@
 
 
 const express = require('express');
-
+const cookieParser = require('cookie-parser');
 const app = express();
 
+app.use(express.static(__dirname + "/public"));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  let randomNumber=Math.round(Math.random());
+  
+  res.cookie('a-b-test',(randomNumber == 0)?'a':'b', { maxAge: 900000 });
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 // listen for requests :)
 app.listen(process.env.PORT || 8080, () => console.log(
